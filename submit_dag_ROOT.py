9e0b31fffb8d6af2ced07efe51414ee2d6c2e6b8
GiacomoDaMolin <giacomo3g@gmail.com>
2022-10-18 17:36:26 +0200

    Longer duration of jobs

--- submit_dag_ROOT.py
+++ submit_dag_ROOT.py
@@ -47,7 +47,7 @@ output                = {base_dir}/out/$(ClusterId).$(ProcId).out\n\
 error                 = {base_dir}/err/$(ClusterId).$(ProcId).err\n\
 log                   = {base_dir}/log/$(ClusterId).$(ProcId).log\n\
 \n\
-+JobFlavour = \"longlunch\"\n\
++JobFlavour = \"tomorrow\"\n\
 {arguments}\n\
 queue"
     return file_str
